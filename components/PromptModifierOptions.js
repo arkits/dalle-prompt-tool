4dd878101114aa37bf9ad28a212bd860e5ea35ef
Archit Khode <arkits@outlook.com>
2022-11-22 23:53:24 -0800

    Initial commit

---
+++ components/PromptModifierOptions.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import Select from "./Select";
+
+export function ModifierOption({ modifier, setModifiers, modifiers }) {
+  const [selected, setSelected] = useState(null);
+
+  /* eslint react-hooks/exhaustive-deps: "off" */
+  useEffect(() => {
+    if (!selected) {
+      return;
+    }
+
+    let updatedModifier = { ...modifier };
+    updatedModifier.selectedOption = selected;
+
+    let updatedModifiers = { ...modifiers };
+    updatedModifiers[updatedModifier?.name] = updatedModifier;
+
+    setModifiers(updatedModifiers);
+  }, [selected]);
+
+  return (
+    <>
+      <Select
+        name={modifier?.name}
+        options={modifier?.options}
+        selected={selected}
+        setSelected={setSelected}
+      />
+    </>
+  );
+}
+
+export default function PromptModifierOptions({ modifiers, setModifiers }) {
+  const modifierOptions = Object.values(modifiers).map((modifier, idx) => {
+    return (
+      <ModifierOption
+        key={idx}
+        modifier={modifier}
+        setModifiers={setModifiers}
+        modifiers={modifiers}
+      />
+    );
+  });
+
+  return (
+    <div className="grid pt-4 gap-4 grid-cols-3 grid-rows-3">
+      {modifierOptions}
+    </div>
+  );
+}
---
+++ components/Select.js
@@ -0,0 +1,146 @@
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Select({ name, options, selected, setSelected }) {
+  return (
+    <div className="flex flex-col">
+      <Listbox value={selected} onChange={setSelected}>
+        {({ open }) => (
+          <>
+            <Listbox.Label className="block text-sm font-medium text-gray-700">
+              {name}
+            </Listbox.Label>
+
+            <div className="relative mt-1">
+              <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+                <span className="flex items-center">
+                  <img
+                    src={selected?.avatar}
+                    width={256}
+                    height={256}
+                    className="h-6 w-6 flex-shrink-0 rounded-full"
+                  />
+                  <span className="ml-3 block truncate">
+                    {selected?.name ? selected?.name : name}
+                  </span>
+                </span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                  <ChevronUpDownIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Listbox.Button>
+
+              <Transition
+                show={open}
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {options?.map((option, idx) => (
+                    <Listbox.Option
+                      key={idx}
+                      className={({ active }) =>
+                        classNames(
+                          active ? "text-white bg-indigo-600" : "text-gray-900",
+                          "relative cursor-default select-none py-2 pl-3 pr-9"
+                        )
+                      }
+                      value={option}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <div className="flex items-center">
+                            <Image
+                              src={option?.avatar}
+                              alt=""
+                              width={256}
+                              height={256}
+                              className="h-6 w-6 flex-shrink-0 rounded-full"
+                            />
+                            <span
+                              className={classNames(
+                                selected ? "font-semibold" : "font-normal",
+                                "ml-3 block truncate"
+                              )}
+                            >
+                              {option.name}
+                            </span>
+                          </div>
+
+                          {selected ? (
+                            <span
+                              className={classNames(
+                                active ? "text-white" : "text-indigo-600",
+                                "absolute inset-y-0 right-0 flex items-center pr-4"
+                              )}
+                            >
+                              <CheckIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+
+                  <Listbox.Option
+                    className={({ active }) =>
+                      classNames(
+                        active ? "text-white bg-indigo-600" : "text-gray-900",
+                        "relative cursor-default select-none py-2 pl-3 pr-9"
+                      )
+                    }
+                    value={null}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <div className="flex items-center">
+                          <Image
+                            width={256}
+                            height={256}
+                            className="h-6 w-6 flex-shrink-0 rounded-full"
+                          />
+                          <span
+                            className={classNames(
+                              selected ? "font-semibold" : "font-normal",
+                              "ml-3 block truncate"
+                            )}
+                          >
+                            None
+                          </span>
+                        </div>
+
+                        {selected ? (
+                          <span
+                            className={classNames(
+                              active ? "text-white" : "text-indigo-600",
+                              "absolute inset-y-0 right-0 flex items-center pr-4"
+                            )}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </>
+        )}
+      </Listbox>
+    </div>
+  );
+}
